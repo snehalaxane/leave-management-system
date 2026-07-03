9ba3a8c2ea13081c8820f6eecbffe431b9bcbd65
SnehaGoexperts <goexpertssneha@gmail.com>
2026-07-03 18:14:54 +0530

    Implement authentication and employee dashboard

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const authenticate = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
+
 const employeeController = require("../controllers/employeeController");
 
-router.get("/dashboard", authenticate, employeeController.getDashboard);
+router.get(
+  "/dashboard",
+  authenticate,
+  authorize("EMPLOYEE"),
+  employeeController.getDashboard,
+);
 
 module.exports = router;
