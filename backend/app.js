6091ae97388fbf3d01d8631a7e5eadf90436c21e
SnehaGoexperts <goexpertssneha@gmail.com>
2026-07-03 16:03:13 +0530

    Initial project setup

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Leave Management API Running...");
+});
+
+module.exports = app;
