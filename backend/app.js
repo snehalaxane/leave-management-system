3d6e1dc98bfda485916024ca8e00a05d9050c2f0
SnehaGoexperts <goexpertssneha@gmail.com>
2026-07-03 16:24:48 +0530

    Configure MySQL connection

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// Database Connection
+require("./config/db");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Leave Management API Running...");
+    res.json({
+        success: true,
+        message: "Leave Management API Running"
+    });
 });
 
 module.exports = app;
